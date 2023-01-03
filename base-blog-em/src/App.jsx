327ca76fceef7390607377ee69975b7a1c611f8e
Dusuna <dusunax@gmail.com>
2023-01-03 23:59:29 +0900

    [init] 소스코드

---
+++ base-blog-em/src/App.jsx
@@ -0,0 +1,25 @@
+import { Posts } from "./Posts";
+import "./App.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
