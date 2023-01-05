805d8ef999f273916465f232cf6ac889b1e9c95f
Dusuna <dusunax@gmail.com>
2023-01-05 23:02:34 +0900

    [Feat] add UpdateMutaion, section 2 ended

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,7 +1,6 @@
 import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
-  console.log(postId);
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
   );
@@ -19,7 +18,7 @@ async function deletePost(postId) {
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "patch post title" } }
+    { method: "PATCH", data: { title: "게시물 제목 수정됨" } }
   );
   return response.json();
 }
@@ -33,7 +32,9 @@ export function PostDetail({ post }) {
 
   // 변이 Mutation
   const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
+  // 조건부 랜더링
   if (isLoading) return <>로딩.......</>;
   if (isError) return <>{error.toString() + "......."}</>;
 
@@ -41,10 +42,20 @@ export function PostDetail({ post }) {
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
 
-      <button>Update title</button>
-      {deleteMutation.isError && <span>삭제 에러 메시지</span>}
-      {deleteMutation.isLoading && <span>삭제 로딩 메시지</span>}
-      {deleteMutation.isSuccess && <span>삭제 성공 메시지</span>}
+      <button onClick={() => deleteMutation.mutate(post.id)}>삭제</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>수정</button>
+
+      {deleteMutation.isError && <p>삭제 에러 메시지</p>}
+      {deleteMutation.isLoading && <p>삭제 로딩 메시지</p>}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>삭제 성공 메시지</p>
+      )}
+
+      {updateMutation.isError && <p>제목 수정 실패!</p>}
+      {updateMutation.isLoading && <p>제목 수정 로딩...</p>}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>제목 수정 성공~</p>
+      )}
 
       <p>{post.body}</p>
       <h4>Comments</h4>
