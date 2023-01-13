fbc9d6b479ce2765903bbc5f2140f97ec173fe30
Dusuna <dusunax@gmail.com>
2023-01-13 23:34:43 +0900

    [Docs & Etc] 실습 및 readme 정리

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,16 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
-// for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+// 비동기 패칭 함수
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -18,11 +19,12 @@ interface UseStaff {
 }
 
 export function useStaff(): UseStaff {
-  // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
-  // TODO: get data from server via useQuery
-  const staff = [];
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
+
+  // console.log(filterByTreatment(staff, filter));
 
   return { staff, filter, setFilter };
 }
